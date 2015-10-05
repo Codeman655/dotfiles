feb7544f957edb4c478cb6b30bee6a5cc79932d6
chricrai <chricrai@cisco.com>
2015-10-05 08:33:38 -0400

    adding additional tooling for push and pull

---
+++ get_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp -r $HOME/.gdbinit .
+cp -r $HOME/.git .
+cp -r $HOME/.git* .
+cp -r $HOME/.oh-my-zsh .
+cp -r $HOME/.vim .
+cp -r $HOME/.vim* .
+cp -r $HOME/.zshrc* .
+
---
+++ install_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp -r .gdbinit $HOME
+cp -r .git $HOME
+cp -r .git* $HOME
+cp -r .oh-my-zsh $HOME
+cp -r .vim $HOME
+cp -r .vim* $HOME
+cp -r .zshrc* $HOME
+
